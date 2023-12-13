bac78ca910012fc9882e9df85f2769e1c066a54c
mesuara <mesuara@gmail.com>
2023-12-13 14:32:24 -0500

    update port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(express.json());
 
 app.get('/api/random', async (req, res) => {
