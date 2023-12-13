9cecda1b4c67e20efec81efcc54f59236f064946
mesuara <mesuara@gmail.com>
2023-12-13 14:31:24 -0500

    API call

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/api/random', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://api.giphy.com/v1/gifs/random',
+      {
+        params: {
+          api_key: 'YOUR_GIPHY_API_KEY',
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching random GIF:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/api/search', async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Search query is required.' });
+  }
+
+  try {
+    const response = await axios.get(
+      'https://api.giphy.com/v1/gifs/search',
+      {
+        params: {
+          api_key: 'YOUR_GIPHY_API_KEY',
+          q: query,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error searching for GIFs:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
